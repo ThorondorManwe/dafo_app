c5b75aa7bb52586f7eabddae50c7559b52d4e1a2
Carlos Rangel <charlesleimaginant@gmail.com>
2024-08-24 18:16:44 -0600

    Initial commit

---
+++ app/page.jsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <h1>Dafo APP</h1>
+      <p>Crea tu DAFO personal facilmente.</p>
+    </main>
+  );
+}
